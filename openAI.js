0a7b8b35c3ff925fd8ec278f50b0e17a7fa68e8d
sono <rhj080471@gmail.com>
2024-07-20 17:52:43 +0900

    Feat : 클라이언트에서 파이어베이스에 업로드된 이미지 url 받아와서 ai에 전달할 수 있도록 구현
    1. 받아온 URL에서 이미지를 다운로드하여 메모리 버퍼로 변환(fetchImageBuffer)
    2. 이미지 버퍼를 webp 형식의 버퍼로 변환(convertToWebpBuffer)
    3. 이미지를 버퍼에서 OpenAI API로 업로드(uploadImageToOpenAI)
    4. 만든 이미지 버퍼를 전달하여 thread 생성(threadResponse)

--- openAI.js
+++ openAI.js
@@ -1,10 +1,83 @@
 require("dotenv").config();
-const fs = require("fs");
 const OpenAI = require("openai");
-const readline = require("readline");
-
+const sharp = require("sharp");
+const axios = require("axios");
+const FormData = require("form-data");
 const openai = new OpenAI();
 
+// 함수는 주어진 URL에서 이미지를 다운로드하여 메모리 버퍼로 반환
+const fetchImageBuffer = async (url) => {
+  const response = await axios({
+    url,
+    responseType: "arraybuffer",
+  });
+  return Buffer.from(response.data, "binary");
+};
+
+// 함수는 이미지 버퍼를 webp 형식의 버퍼로 변환
+const convertToWebpBuffer = async (buffer) => {
+  return sharp(buffer).toFormat("webp").toBuffer();
+};
+
+// 함수는 form-data를 사용하여 이미지를 버퍼에서 OpenAI API로 업로드
+const uploadImageToOpenAI = async (buffer) => {
+  const form = new FormData();
+  console.log("buffer", buffer);
+  form.append("file", buffer, { filename: "image.webp" });
+  form.append("purpose", "assistants");
+
+  const response = await axios.post("https://api.openai.com/v1/files", form, {
+    headers: {
+      ...form.getHeaders(),
+
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    },
+  });
+
+  return response.data;
+};
+
+// assistants 대화 Thread 생성
+const createThread = async (imageUrl) => {
+  // Fetch the image as a buffer
+  const imageBuffer = await fetchImageBuffer(imageUrl);
+
+  //  Convert the image buffer to WebP format
+  const webpBuffer = await convertToWebpBuffer(imageBuffer);
+
+  try {
+    // Upload the WebP image buffer to OpenAI
+    const fileResponse = await uploadImageToOpenAI(webpBuffer);
+    const fileId = fileResponse.id;
+
+    // Create a thread with the uploaded image and text
+    const threadResponse = await openai.beta.threads.create({
+      messages: [
+        {
+          role: "user",
+          content: [
+            {
+              type: "image_file",
+              image_file: {
+                file_id: fileId,
+              },
+            },
+            {
+              type: "text",
+              text: "내 전기 요금 고지서야, 이 고지서를 분석해줘.",
+            },
+          ],
+        },
+      ],
+    });
+
+    return threadResponse.id;
+  } catch (error) {
+    console.error("Error creating thread:", error);
+  }
+};
+
+// assistants에 메세지 보내고 응답 받는 함수
 const sendMessage = async (imageUrl, content, threadId, isCreateThread) => {
   return new Promise(async (resolve, reject) => {
     try {
@@ -56,39 +129,6 @@ const sendMessage = async (imageUrl, content, threadId, isCreateThread) => {
       reject({ answer: null, answerCitations: null, err: error });
     }
   });
-};
-
-const createThread = async (imageUrl) => {
-  try {
-    const file = await openai.files.create({
-      file: fs.createReadStream(imageUrl),
-      purpose: "assistants",
-    });
-
-    const thread = await openai.beta.threads.create({
-      messages: [
-        {
-          role: "user",
-          content: [
-            {
-              type: "image_file",
-              image_file: {
-                file_id: file.id,
-              },
-            },
-            {
-              type: "text",
-              text: "내 전기 요금 고지서야, 이 고지서를 분석해줘.",
-            },
-          ],
-        },
-      ],
-    });
-
-    return thread.id;
-  } catch (error) {
-    console.error("Error creating thread:", error);
-  }
 };
 
 module.exports = {
